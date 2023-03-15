79e50d486cad431995bc055af16159b7294dd126
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2023-03-15 14:34:38 -0600

    user service ready

---
+++ user-service/src/main/java/com/michael/userservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.michael.userservice.repository;
+
+import com.michael.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByName(String name);
+
+}

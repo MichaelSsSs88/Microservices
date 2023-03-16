677af9c751ce5ec16569c36285ac42fa612e9cbe
MichaelSsSs88 <63695255+MichaelSsSs88@users.noreply.github.com>
2023-03-16 16:25:35 -0600

    a
    a

--- bike-service/src/main/java/com/michael/bikeservice/BikeServiceApplication.java
+++ bike-service/src/main/java/com/michael/bikeservice/BikeServiceApplication.java
@@ -2,7 +2,9 @@ package com.michael.bikeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BikeServiceApplication {
 
--- car-service/src/main/java/com/michael/carservice/CarServiceApplication.java
+++ car-service/src/main/java/com/michael/carservice/CarServiceApplication.java
@@ -2,7 +2,9 @@ package com.michael.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CarServiceApplication {
 
